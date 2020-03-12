b753308e86ed3fbffae08894b8f1428f9d9c0a4c
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-12 20:07:47 +0700

    Tugas 8 JS

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function tugasDelapan(){
+    var motor = {
+        brand: 'Yamaha',
+        nama: 'Vixion',
+        tahun: 2018,
+        harga: 30000000,
+    };
+    
+    motor.tahun = 2015;
+    console.log(motor);
+}
+
+tugasDelapan();
